609e2dda60ab7c5e909094d1c5be3253d17ad372
tmwalker.nyc@gmail.com <clard1970@gmail.com>
2016-07-21 23:04:21 -0400

    initial commit

---
+++ application/views/welcome.php
@@ -0,0 +1,108 @@
+<!--iWERKT version 1.1-->
+
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>iWERKT</title>
+
+      <link rel='stylesheet' type='text/css' href='/iWerkt-master/css/styles.css'>
+      <link rel="stylesheet" href="/iWerkt-master/css/animate.css">
+      <link rel="stylesheet" href='/iWerkt-master/css/font-awesome-4.6.3/css/font-awesome.min.css'>
+      <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js'></script>
+      
+  </head>
+  <body>
+    <div class='header'>
+      <div class='nav'>
+        <ul>
+          <li><a href='https://itunes.apple.com/us/app/iwerkt/id1097698969?mt=8'>iOS</a><span>|</span><a href='https://play.google.com/store/apps/details?id=com.ivymdl.iwerkt'>Android</a></li>
+        </ul>
+      </div>
+      <div class='logoBlock'>
+        <div class='logoContainer'>
+          <div class='logo'><a href='https://itunes.apple.com/us/app/iwerkt/id1097698969?mt=8'>iWERKT</a></div>
+          <p class='logoBody' id='bodyText'>Digital Vouchers for Models</p>
+        </div>
+      </div>
+    </div>
+    <div class='cycle-slideshow'>
+    </div>
+    <div class='noPaperSection'>
+      <div class='noPaperSectionContainer'>
+        <div class='titleImg01'></div>
+        <div class='titleImg02'></div>
+        <div class='titleText' id="noPaperSectionTitle">No paper.</div>
+        <p class='noPaperSectionBody' id='bodyText'>Finally, no more voucher books.<br> No more piles of fly-away papers. No lost vouchers. No waiting around until you have the chance to get them to your agency.<br> Get your client's signature right on your phone, hit send, and go!</p>
+      </div>
+    </div>
+    <div class='keepTrackSection'>
+      <div class='keepTrackSectionContainer'>
+        <div class='titleText' id='keepTrackSectionTitle'>Keep track of everything.</div>
+        <p class='keepTrackSectionBody' id='bodyText'>Track your income, keep tabs on whether or not you've been paid for a job, maintain client information, and export your data for tax prep. 
+        <br>
+        <br>
+        All in one APP.
+        </p>
+        <div class='keepTrackImgContainer'>
+          <span class='keepTrackImgs' id='keepTrackImg01'></span>
+          <span class='keepTrackImgs' id='keepTrackImg02'></span>
+           <span class='keepTrackImgs'id='keepTrackImg03'></span>
+        </div>
+      </div>
+    </div>
+    <div class='getPaidSection'>
+      <div class='getPaidSectionContainer'>
+        <div class='getPaidImg01'></div>
+        <div class='getPaidSectionBlock'>
+        <div class='titleText wow bounceInDown' id='getPaidSectionTitle'>Get paid faster.</div>
+        <p class='getPaidSectionBody' id='bodyText'>Instant delivery.<br> Fewer mistakes.<br> No math to do.<br> No illegible vouchers.<br>No lost vouchers.<br><br>With iWerkt, your agency's accounting department will now be able to start their billing process right away - and that means you'll get paid faster! 
+        </p>
+        </div>
+      </div>
+    </div>
+    <div class='getAppSection'>
+      <div class='getAppSection'>
+        <div class='titleText' id='getAppSectionTitle'><a href='https://itunes.apple.com/us/app/iwerkt/id1097698969?mt=8'>Get the app.</a></div>
+        <p class='getAppSectionBody' id='bodyText'>(it's free)</p>
+          <div class='getAppLinksLarge'>
+            <div class='getAppIconsLarge'>
+              <a href='https://itunes.apple.com/us/app/iwerkt/id1097698969?mt=8'>
+                <div class='iosAppIconLarge'></div>
+              </a>
+              <a href='https://play.google.com/store/apps/details?id=com.ivymdl.iwerkt'>
+                <div class='androidAppIconLarge'></div>
+              </a>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class='footer'>
+      <div class='leftSide'>
+        <span class='copyright'><a href='#'>&copy NordikLabs</a></span>
+        <span class='footerLink'><a href='/privacy.html'>Privacy</a></span>
+        <span class='footerLink'><a href='/terms.html'>Terms of Use</a></span>
+      </div>   
+      <div class='rightSide'>     
+        <span class='footerLink'><a href='http://eepurl.com/b57Xn1'>Subscribe</a></span>  
+        <span class='footerLink'><a href='mailto:&#105;&#110;&#102;&#111;&#64;&#105;&#119;&#101;&#114;&#107;&#116;&#46;&#99;&#111;&#109'>Contact</a></span>
+        <span class='footerLink'><a href='#'><i class='fa fa-facebook'></i></a></span>
+        <span class='footerLink'><a href='#'><i class='fa fa-twitter'></i></a></span>
+      </div>
+    </div>
+    
+    <script type='text/javascript'>
+      $('.cycle-slideshow').backstretch(
+        ['/iWerkt-master/css/img/girlHorizontale.jpg','/iWerkt-master/css/img/5thAve.jpg','/iWerkt-master/css/img/birdsNest.jpg','/iWerkt-master/css/img/notebookPhone.jpg','/iWerkt-master/css/img/flowerGirl2.jpg','/iWerkt-master/css/img/phoneOnTable.jpg','/iWerkt-master/css/img/tightRoller.jpg'], {duration: 3000, fade: 1500}
+      );
+    </script>
+
+    <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false">
+    </script>
+
+    <script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us13.list-manage.com","uuid":"96d586c11b35298a962d197d1","lid":"16bb80dc65"}) })
+    </script>
+
+
+  </body>
+</html>
